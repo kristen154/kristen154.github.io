7fe36de21fe83941716c6179710e92bc502778d2
kristen1544 <847191061@qq.com>
2019-03-12 21:31:58 +0800

    js/css test

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/* 
+* @Author: anchen
+* @Date:   2019-03-12 21:30:31
+* @Last Modified by:   anchen
+* @Last Modified time: 2019-03-12 21:30:44
+*/
+
+$(document).ready(function(){
+    console.log("hello world")
+});
